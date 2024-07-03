f81de79bf5b0a10fac8e9021f3a076b0447e52b1
inhokee <kee.inho0@gmail.com>
2024-07-03 01:37:46 -0400

    update 3d plot

--- safety_loss_function_example.py
+++ safety_loss_function_example.py
@@ -51,7 +51,45 @@ def plot_safety_loss_function(tracking_controller):
     plt.legend()
     plt.show()
 
+
+
 def plot_safety_loss_function_grid(tracking_controller):
+    if not tracking_controller.data_generation:
+        return
+
+    alpha_obs_values = [1.5, 1.0, 0.5]
+    delta_theta_values = [-0.1, -0.7, -1.4, -2.1]
+    beta_obs = 0.5
+    fig, axs = plt.subplots(3, 4, figsize=(15, 15), subplot_kw={'projection': '3d'})
+    plt.subplots_adjust(hspace=0.4, wspace=0.4)
+
+    x_range = np.linspace(tracking_controller.robot.X[0] - 10, tracking_controller.robot.X[0] + 5, 30)
+    y_range = np.linspace(tracking_controller.robot.X[1] - 5, tracking_controller.robot.X[1] + 5, 30)
+    X, Y = np.meshgrid(x_range, y_range)
+
+    for j, delta_theta in enumerate(delta_theta_values):
+        for i, alpha_obs in enumerate(alpha_obs_values):
+            Z = np.zeros_like(X)
+            for m in range(X.shape[0]):
+                for n in range(X.shape[1]):
+                    z = np.array([X[m, n], Y[m, n]])
+                    obs = {
+                        'z': tracking_controller.near_obs[:2].flatten(),
+                        'h': tracking_controller.robot.agent_barrier(tracking_controller.robot.X, np.array([0, 0]), tracking_controller.gamma1, tracking_controller.gamma2, tracking_controller.dt, tracking_controller.robot.robot_radius, tracking_controller.near_obs),
+                        'd': delta_theta
+                    }
+                    Z[m, n] = compute_safety_loss_function(obs, alpha_obs, beta_obs, tracking_controller.gamma_loss, tracking_controller.lambda_loss, z)
+
+            ax = axs[i, j]
+            ax.plot_surface(X, Y, Z, cmap='viridis')
+            ax.set_title(f'alpha_obs = {alpha_obs}, delta_theta = {delta_theta}')
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Safety Loss Function')
+
+    plt.show()
+
+def plot_safety_loss_function_grid_2d(tracking_controller):
     if not tracking_controller.data_generation:
         return
 
